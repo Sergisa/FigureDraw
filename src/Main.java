5e305f85c8a50375dae50f7f25f5fe1e153c7270
sergisa <phebra@yandex.ru>
2020-10-20 15:58:36 +0400

    Processing added

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,59 @@
 import geometry.*;
+import processing.core.PApplet;
 
 import java.awt.*;
 import java.util.*;
 import java.util.List;
 
-public class Main {
+public class Main extends PApplet {
     private static Dot rotationRelativeDot, rotatingDot, rotatedDot;
     private static List<GeomteryFigure> figures;
     private static Line line1;
     private static Line line2;
 
+    public void settings()
+    {
+        size(500,500);
+    }
+
+    public void setup(){
+        //size(500,500);
+    }
+
+    void drawGrid()
+    {
+        stroke( 225 );
+        for ( int i = 0; i < 64; i++ ) {
+            line(i * 10, 0, i * 10, height );
+        }
+        for ( int i = 0; i < 48; i++ ) {
+            line( 0, i * 10, width, i * 10 );
+        }
+    }
+
+    public void mousePressed()
+    {}
+
+    public void mouseReleased()
+    {}
+
+    public void keyPressed()
+    {
+        loop();
+    }
+
+    public void draw()
+    {
+        drawGrid();
+    }
+
     static public void main(String[] passedArgs) {
+        String[] appletArgs = new String[] { "Main" };
+        if (passedArgs != null) {
+            PApplet.main(concat(appletArgs, passedArgs));
+        } else {
+            PApplet.main(appletArgs);
+        }
         //Drawer drawer = new Drawer(this);
         figures = new ArrayList<>();
         rotationRelativeDot = new Dot(100,100);
@@ -34,6 +77,5 @@ public class Main {
         for(GeomteryFigure figure:figures) {
             System.out.println(figure.getClass().getSuperclass().getName() +" ["+ figure.getClass().getName()+"] "+figure.getName());
         }
-        MainForm frm = new MainForm();
     }
 }

eb1cb880463aec5ac9323314ca232971d75b683d
sergisa <phebra@yandex.ru>
2020-11-06 20:37:32 +0300

    Builder and getters and setters added to Line class

--- src/geometry/Line.java
+++ src/geometry/Line.java
@@ -1,20 +1,51 @@
 package geometry;
 
+import java.awt.*;
 import java.util.Objects;
 
 public class Line extends GeomteryObject {
-    public Dot point1, point2;
+    public Dot point1;
+    public Dot point2;
     public Line(Dot point1, Dot point2){
         //super(name);
         this.point1 = point1;
         this.point2 = point2;
     }
 
-    public static Line create(Dot dot1, Dot dot2){
-        return new Line(dot1, dot2);
+    private Line(Builder builder) {
+        setColor(builder.color);
+        point1 = builder.point1;
+        point2 = builder.point2;
     }
-    public static Line create(float[] a, float[] b){
-        return create( new Dot(a[0],a[1]), new Dot(b[0],b[1]) );
+
+    public Dot getPoint1() {
+        return point1;
+    }
+
+    public Line setPoint1(Dot point1) {
+        this.point1 = point1;
+        return this;
+    }
+
+    public Dot getPoint2() {
+        return point2;
+    }
+
+    public Line setPoint2(Dot point2) {
+        this.point2 = point2;
+        return this;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static Builder newBuilder(Line copy) {
+        Builder builder = new Builder();
+        builder.color = copy.getColor();
+        builder.point1 = copy.getPoint1();
+        builder.point2 = copy.getPoint2();
+        return builder;
     }
 
     public Dot getCenter(){
@@ -45,5 +76,33 @@ public class Line extends GeomteryObject {
     @Override
     public int hashCode() {
         return Objects.hash(point1, point2);
+    }
+
+    public static final class Builder {
+        private Color color;
+        private Dot point1;
+        private Dot point2;
+
+        private Builder() {
+        }
+
+        public Builder setColor(Color color) {
+            this.color = color;
+            return this;
+        }
+
+        public Builder setPoint1(Dot point1) {
+            this.point1 = point1;
+            return this;
+        }
+
+        public Builder setPoint2(Dot point2) {
+            this.point2 = point2;
+            return this;
+        }
+
+        public Line build() {
+            return new Line(this);
+        }
     }
 }
